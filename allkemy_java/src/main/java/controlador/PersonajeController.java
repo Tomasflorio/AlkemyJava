106375e21969c2d05b40d8a77346c15b24671b14
Tomasflorio <tomasflorio00@gmail.com>
2022-02-22 14:28:23 -0300

    personaje controller

--- allkemy_java/src/main/java/controlador/PersonajeController.java
+++ allkemy_java/src/main/java/controlador/PersonajeController.java
@@ -1,15 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controlador;
+
+import java.awt.Image;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.Personaje;
+import modelo.PersonajeDAO;
 
 /**
  *
- * @author tomas
+ * @author tomas florio
  */
-public class PersonajeController {
-    
-    
+@WebServlet(name = "PersonajeController", urlPatterns = {"/PersonajeController"})
+public class PersonajeController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PersonajeDAO personajeDAO = new PersonajeDAO();
+        String accion;
+        RequestDispatcher dispatcher;
+        dispatcher = null;
+        accion = request.getParameter("accion");
+        if (accion == null || accion.isEmpty()) {
+            dispatcher = request.getRequestDispatcher("/");
+        } else if (accion.equals("modificar")) {
+            dispatcher = request.getRequestDispatcher("Vistas/modificar.jsp");
+        } else if (accion.equals("actualizar")) {
+            int id_per = Integer.parseInt(request.getParameter("id_per")); //fijarse que este bien el id
+            String nombre = request.getParameter("nombre");
+            String historia = request.getParameter("historia");
+            int edad = Integer.parseInt(request.getParameter("edad"));
+            int peso = Integer.parseInt(request.getParameter("peso"));
+            int id_pelicula = Integer.parseInt(request.getParameter("pelaso"));//fijarse que este bien 
+            Image imagen = (Image) request.getAttribute("imagen");
+            Personaje perso = new Personaje(id_per, nombre, edad, peso, historia, id_pelicula, imagen);
+            personajeDAO.actualizarPersonaje(perso);
+            dispatcher = request.getRequestDispatcher("Vistas/character.jsp");
+        } else if (accion.equals("nuevo")) {
+            dispatcher = request.getRequestDispatcher("Vistas/nuevo.jsp");
+        } else if (accion.equals("insert")) {
+            int id_per = Integer.parseInt(request.getParameter("id_per")); //fijarse que este bien el id
+            String nombre = request.getParameter("nombre");
+            String historia = request.getParameter("historia");
+            int edad = Integer.parseInt(request.getParameter("edad"));
+            int peso = Integer.parseInt(request.getParameter("peso"));
+            int id_pelicula = Integer.parseInt(request.getParameter("pelaso"));//fijarse que este bien 
+            Image imagen = (Image) request.getAttribute("imagen");
+            Personaje perso = new Personaje(id_per, nombre, edad, peso, historia, id_pelicula, imagen);
+            personajeDAO.insertarPersonaje(perso);
+            dispatcher = request.getRequestDispatcher("Vistas/character.jsp");
+        } else if (accion.equals("eliminar")) {
+            int id = Integer.parseInt(request.getParameter("id"));// fijarse nombre id
+            personajeDAO.eliminarPersonaje(id);
+            dispatcher = request.getRequestDispatcher("Vistas/characters.jsp");
+        } else {
+            dispatcher = request.getRequestDispatcher("/index.html");
+        }
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
 }
--- allkemy_java/src/main/java/controlador/UsuariosController.java
+++ allkemy_java/src/main/java/controlador/UsuariosController.java
@@ -14,40 +14,49 @@ import modelo.Usuario;
  *
  * @author tomas florio
  */
-
-@WebServlet(name="UsuariosController",urlPatterns={"/UsuariosController"}) 
+@WebServlet(name = "UsuariosController", urlPatterns = {"/UsuariosController"})
 public class UsuariosController extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException , IOException{
+            throws ServletException, IOException {
         UsersDAO userDAO = new UsersDAO();
         String accion;
         RequestDispatcher dispatcher = null;
         accion = request.getParameter("accion");
-        if(accion == null || accion.isEmpty()){
+        if (accion == null || accion.isEmpty()) {
             dispatcher = request.getRequestDispatcher("/");
-        }else if(accion.equals("crear")){
+        } else if (accion.equals("crear")) {
             dispatcher = request.getRequestDispatcher("Vistas/register.jsp");
-        }else if(accion.equals("insertarUser")){
+        } else if (accion.equals("insertarUser")) {
             String nombre = request.getParameter("nombre");
             String apellido = request.getParameter("apellido");
             String email = request.getParameter("email");
             String passw = request.getParameter("passw");
-            Usuario usr = new Usuario(0,nombre,apellido,email,passw);            
+            Usuario usr = new Usuario(0, nombre, apellido, email, passw);
             userDAO.registerUser(usr);
             dispatcher = request.getRequestDispatcher("/index.html");
-        }else if(accion.equals("ingresar")){
+        } else if (accion.equals("ingresar")) {
             String usuario = request.getParameter("email");
             String passw = request.getParameter("passw");
-            boolean ingresar = userDAO.ingresarUser(usuario,passw);
-            if(ingresar){
+            boolean ingresar = userDAO.ingresarUser(usuario, passw);
+            if (ingresar) {
                 dispatcher = request.getRequestDispatcher("Vistas/characters.jsp");
-            }else{
+            } else {
                 dispatcher = request.getRequestDispatcher("/index.html");
             }
         }
         dispatcher.forward(request, response);
-                
+
     }
-    
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
 }
